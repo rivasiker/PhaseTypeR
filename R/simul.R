9d413fe56c0e4fef28a215f03638f80145953c0a
Lars Nørvang Andersen <au149533@uni.au.dk>
2020-07-09 07:50:48 +0200

    Cleaned up code for simulation

--- R/simul.R
+++ R/simul.R
@@ -3,13 +3,12 @@
 #' sim_phase_type implements a naive simulation scheme for simulation of
 #' phase-type distributions.
 #'
-#' @value A list containing a first vector with the the time in each state and
-#' a second vector with the state path before reaching the absorbing state.
 #'
 #' @param obj an object of class \code{disc_phase_type} or
 #' \code{cont_phase_type}
+#' @param R Number of replications
 #'
-#' @usage sim_phase_type
+#' @usage sim_phase_type <- function(obj)
 #'
 #' @examples
 #'
@@ -45,7 +44,8 @@
 #'                                2, -2, 0,
 #'                                0, 1, -1), nrow = 3, ncol = 3)
 #' reward_matrix = matrix(sample(seq(0, 10, 0.1), 6), nrow = 3, ncol = 2)
-#' phase_type(subintensity_matrix, reward_mat = reward_matrix)
+#' obj <- phase_type(subintensity_matrix, reward_mat = reward_matrix)
+#' sim_phase_type(obj)
 #'
 #' ##===========================##
 #' ## For discrete multivariate ##
@@ -59,17 +59,39 @@
 #'
 #' @export
 
-sim_phase_type <- function(obj){
+sim_phase_type <- function(obj, R = 100){
   if (!(class(obj) == 'cont_phase_type') &&
-      !(class(obj) == 'disc_phase_type')) {
+      !(class(obj) == 'disc_phase_type') &&
+      !(class(obj) == 'mult_cont_phase_type') &&
+      !(class(obj) == 'mult_disc_phase_type')){
     stop("Please provide an object of 'class disc_phase_type' or", "
          'cont_phase_type'")
   }
 
   init_probs <- obj$init_probs
   n <- length(init_probs)
-
   subint_mat <- obj$subint_mat
+
+  if ((class(obj) == 'mult_cont_phase_type') ||
+      (class(obj) == 'mult_disc_phase_type')){
+    reward_mat <- obj$reward_mat
+    p <- ncol(reward_mat)
+    out <- matrix(0, n, R)
+    core_ph <- phase_type(subint_mat, init_probs)
+    for(i in 1:R) {
+      smph <- sim_phase_type(core_ph); #Simulate a single path
+      pointwiserewards <- matrix(0, n, length(smph[[1]])) # a vector to hold the rewards obtained for each state (no reward is obtained in the aborbing state so length is that of the inter-jump-times)
+      # right now, smph[[2]] holds a rowvector of the states visited prior to in incl. absorbtion
+      #
+      for(j in 1:p) {
+        pointwiserewards[, which(smph[[2]] == j)] <- reward_mat[, j]
+      }
+      #calculate the rewards times the time spent in each state using point-wise multiplication
+      out[,i] <- rowSums(pointwiserewards * matrix(smph[[1]], n, length(smph[[1]]), byrow = TRUE))
+    }
+    return(out)
+  }
+
   if (class(obj) == ('cont_phase_type')){
     exit_rate <- -subint_mat %*% matrix(1, n, 1)
   } else {
@@ -80,10 +102,9 @@ sim_phase_type <- function(obj){
   states <- 1:(n+1)
   curstate <- sample(1:n, 1, prob = init_probs) #sample initial state
   states <- curstate
+  times <- NULL
 
   if(class(obj) == 'cont_phase_type'){
-    curtime <- 0 #current time, starts at 0
-    times <- 0
     while(curstate <= n){
       curtime <- rexp(1, -subint_mat[curstate, curstate])
       curstate <- sample((1:(n + 1))[-curstate], 1,
@@ -92,7 +113,6 @@ sim_phase_type <- function(obj){
       states <- c(states, curstate)
     }
   } else {
-    times <- NULL
     curtime <- 1
     while(curstate <= n){
       curstate <- sample(1:(n+1), 1, prob = int_mat[curstate,])
@@ -117,15 +137,19 @@ sim_phase_type <- function(obj){
 
 ##########################################
 
-# mvrewsim multivariate rewards via simulation
+#' sim_rew_phase_type implements a naive simulation scheme for simulation of
+#' multivariate phase-type distributions.
 
-# Input: R - number of replications
-# reward_mat Rewards matrix p times n - if a vector is provided it is converted to a matrix and univariate rewards are assumed
-# the number of columns of reward_mat must match subint_mat
+#' @param R - number of replications
+#' @param obj an object of class \code{mult_cont_phase_type} or
+#' \code{cont_phase_type}
+#' @param R reward_mat A p times n matrix of rewards. If a vector is provided it is converted to a matrix and univariate rewards are assumed the number of columns of reward_mat must match subint_mat
 
-# output: An n times R vector
+#' @return An n times R vector
 
-mvrewsim <- function(R, obj, reward_mat = NULL) {
+#' @export
+
+sim_rew_phase_type <- function(R, obj, reward_mat = NULL) {
   if (!(class(obj) == 'mult_cont_phase_type' || class(obj) == 'cont_phase_type')) {
     stop("Please provide an object of type cont_phase_type or mult_cont_phase_type")
   }
@@ -145,15 +169,17 @@ mvrewsim <- function(R, obj, reward_mat = NULL) {
   if (p != dim(subint_mat)[1]) {stop("Dimension mismatch between subint_mat and reward_mat")}
   out <- matrix(0, n, R)
   for(i in 1:R) {
-    smph <- naivesimph(ph_obj); #Simulate a single path
-    pointwiserewards <- matrix(0, n, length(smph[[1]])) # a vector to hold the rewards oexit_rateained for each state (no reward is oexit_rateained in the aborbing state so length is that of the inter-jump-times)
-    # right now, smph[[2]] holds a rowvector of the states visited prior to in incl. absorexit_rateion
+    smph <- sim_phase_type(ph_obj); #Simulate a single path
+    pointwiserewards <- matrix(0, n, length(smph[[1]])) # a vector to hold the rewards obtained for each state (no reward is obtained in the aborbing state so length is that of the inter-jump-times)
+    # right now, smph[[2]] holds a rowvector of the states visited prior to in incl. absorption
     #
     for(j in 1:p) {
       pointwiserewards[, which(smph[[2]] == j)] <- reward_mat[j,]
     }
     #calculate the rewards times the time spent in each state using point-wise multiplication
-    out[,i] <- rowSums(pointwiserewards * matrix(smph[[1]], n, length(smph[[1]]), byrow = TRUE))
+    out[,i] <- rowSums(pointwiserewards * matrix(smph[[1]], n,
+                                                 length(smph[[1]]), byrow = TRUE))
   }
   return(out)
 }
+
