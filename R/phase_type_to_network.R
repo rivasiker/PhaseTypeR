38c1207ac27263062d8f6ebd58333a29485875a8
Iker <ikerrivas96@gmail.com>
2021-06-01 15:29:58 +0200

    Change coloring of graph

--- R/phase_type_to_network.R
+++ R/phase_type_to_network.R
@@ -35,7 +35,7 @@ phase_type_to_network <- function(phase_type, t = NULL) {
     id_mat <-
       data.frame(
         id = paste0('V', 0:(nrow(x)-1)),
-        color = c('green', rep('gray80', nrow(x)-2), 'red')
+        color = c('chartreuse3', rep('gray80', nrow(x)-2), 'red')
       )
 
 
@@ -45,40 +45,53 @@ phase_type_to_network <- function(phase_type, t = NULL) {
 
     mat <- phase_type$subint_mat
     x <- rbind(cbind(mat, -rowSums(mat)), c(rep(0, nrow(mat)+1)))
-    print(x)
+
     if (is.numeric(t)) {
       x <- round(expm(x*t), 3)
-      if (t != 0) {
+      if (t == 0) {
         x <- rbind(c(phase_type$init_probs, 1-sum(phase_type$init_probs)), x)
         x <- cbind(rep(0, nrow(x)), x)
         colnames(x) <- paste0('V', 0:(nrow(x)-1))
         rownames(x) <- colnames(x)
+        id_mat <-
+          data.frame(
+            id = paste0('V', 0:(nrow(x)-1)),
+            color = c('chartreuse3', rep('gray80', nrow(x)-2), 'tomato')
+          )
       } else {
-        colnames(x) <- paste0('V', 1:(nrow(x)-1))
+        colnames(x) <- paste0('V', 1:(nrow(x)))
         rownames(x) <- colnames(x)
-        }
+        id_mat <-
+          data.frame(
+            id = paste0('V', 1:(nrow(x))),
+            color = c(rep('gray80', nrow(x)-1), 'tomato')
+          )
+      }
 
     } else if (is.null(t)) {
       x <- rbind(c(phase_type$init_probs, 1-sum(phase_type$init_probs)), x)
       x <- cbind(rep(0, nrow(x)), x)
       colnames(x) <- paste0('V', 0:(nrow(x)-1))
       rownames(x) <- colnames(x)
+      id_mat <-
+        data.frame(
+          id = paste0('V', 0:(nrow(x)-1)),
+          color = c('chartreuse3', rep('gray80', nrow(x)-2), 'tomato')
+        )
+
     } else {
       stop('Please provide a numeric time or NULL')
     }
 
-
-    link_mat <- data.frame(from=rownames(x)[row(x)], to=colnames(x)[col(x)],  weight=c(x))
+    link_mat <- data.frame(from=rownames(x)[row(x)], to=colnames(x)[col(x)],
+                           weight=c(x))
     link_mat <- link_mat[link_mat$weight != 0,]
 
-    id_mat <-
-      data.frame(
-        id = paste0('V', 0:(nrow(x)-1)),
-        color = c('tomato', rep('gray80', nrow(x)-2), 'gold')
-      )
+
 
 
   }
+
 
   graph_from_data_frame(d=link_mat, vertices=id_mat, directed=T)
 }
