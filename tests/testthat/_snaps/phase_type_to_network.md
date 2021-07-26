97b573cda176ff5c83bdd72e0bc5d9f2f1e38fdf
Iker <ikerrivas96@gmail.com>
2021-07-26 16:38:13 +0200

    test phase-type to network

---
+++ tests/testthat/_snaps/phase_type_to_network.md
@@ -0,0 +1,38 @@
+# networks in the continuous phase-type distribution
+
+    Code
+      phase_type_to_network(cont_phase_type)[1:5]
+    Output
+      5 x 5 sparse Matrix of class "dgCMatrix"
+         V0   V1   V2   V3  V4
+      V0  .  1.0  .    .   .  
+      V1  . -1.5  1.5  .   .  
+      V2  .  .   -1.0  1.0 .  
+      V3  .  .    .   -0.5 0.5
+      V4  .  .    .    .   .  
+
+---
+
+    Code
+      phase_type_to_network(cont_phase_type, 3)[1:4]
+    Output
+      4 x 4 sparse Matrix of class "dgCMatrix"
+            V1    V2    V3    V4
+      V1 0.011 0.116 0.404 0.469
+      V2 .     0.050 0.347 0.604
+      V3 .     .     0.223 0.777
+      V4 .     .     .     1.000
+
+# networks in the discrete phase-type distribution
+
+    Code
+      phase_type_to_network(disc_phase_type)[1:5]
+    Output
+      5 x 5 sparse Matrix of class "dgCMatrix"
+         V0  V1  V2  V3  V4
+      V0  . 0.7 0.3 .   .  
+      V1  . .   0.2 0.8 .  
+      V2  . 0.5 0.5 .   .  
+      V3  . .   .   0.4 0.6
+      V4  . .   .   .   1.0
+
---
+++ tests/testthat/test-phase_type_to_network.R
@@ -0,0 +1,35 @@
+context('Phase-type distributions to networks')
+
+library(testthat)
+local_edition(3)
+library(PhaseTypeR)
+
+cont_phase_type <- PH(subint_mat = matrix(c(-1.5, 0, 0,
+                                            1.5, -1, 0,
+                                            0, 1, -0.5), ncol = 3),
+                      init_probs = c(1, 0, 0))
+
+test_that(
+  'networks in the continuous phase-type distribution', {
+    expect_snapshot(phase_type_to_network(cont_phase_type)[1:5])
+    expect_snapshot(phase_type_to_network(cont_phase_type, 3)[1:4])
+    expect_error(phase_type_to_network(cont_phase_type, 'a'),
+                 'time or NULL')
+  }
+)
+
+
+
+disc_phase_type <- DPH(matrix(c(0, 0.2, 0.8,
+                                0.5, 0.5, 0,
+                                0, 0, 0.4), ncol = 3, byrow = T),
+                       c(0.7, 0.3, 0))
+
+
+test_that(
+  'networks in the discrete phase-type distribution', {
+    expect_snapshot(phase_type_to_network(disc_phase_type)[1:5])
+    expect_warning(phase_type_to_network(disc_phase_type, 3)[1:5],
+                   'Argument t not used')
+  }
+)
