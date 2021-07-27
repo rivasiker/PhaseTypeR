0a70d0dd3826cd7086d6f820f1fe0051b479edf9
Iker <ikerrivas96@gmail.com>
2021-07-27 11:44:10 +0200

    finish var tests

--- tests/testthat/test-mean_var.R
+++ tests/testthat/test-mean_var.R
@@ -136,6 +136,30 @@ test_that(
                           init_probs = c(1, 0, 0))),
                  matrix(c(36.25, 76, 76, 160.75), nrow = 2))
 
+    expect_equal(var(MPH(subint_mat = matrix(c(-2, 0, 0,
+                                               1, -1, 0,
+                                               0, 1, -0.5), ncol = 3),
+                         reward_mat = matrix(c(1, 2, 3, 4, 5, 6), nrow = 3, ncol = 2),
+                         init_probs = c(1, 0, 0)),
+                     v = 2),
+                 160.75)
+
+    expect_equal(var(MPH(subint_mat = matrix(c(-2, 0, 0,
+                                               1, -1, 0,
+                                               0, 1, -0.5), ncol = 3),
+                         reward_mat = matrix(c(1, 2, 3, 4, 5, 6), nrow = 3, ncol = 2),
+                         init_probs = c(1, 0, 0)),
+                     v = c(1, 2)),
+                 76)
+
+    expect_error(var(MPH(subint_mat = matrix(c(-2, 0, 0,
+                                               1, -1, 0,
+                                               0, 1, -0.5), ncol = 3),
+                         reward_mat = matrix(c(1, 2, 3, 4, 5, 6), nrow = 3, ncol = 2),
+                         init_probs = c(1, 0, 0)),
+                     v = c(1, 2, 1)),
+                 'right indices')
+
     expect_equal(var(MDPH(subint_mat = matrix(c(0.4, 0.24, 0.12,
                                                  0,   0.4,  0.2,
                                                  0,   0,    0.5),
@@ -144,4 +168,54 @@ test_that(
                            reward_mat = matrix(c(1, 2, 3, 4, 5, 6), nrow = 3, ncol = 2),
                            init_probs = c(1, 0, 0))),
                  matrix(c(626/9, 1652/9, 1652/9, 4442/9), nrow = 2))
+
+    expect_equal(var(MDPH(subint_mat = matrix(c(0.4, 0.24, 0.12,
+                                                0,   0.4,  0.2,
+                                                0,   0,    0.5),
+                                              ncol = 3,
+                                              byrow = TRUE),
+                          reward_mat = matrix(c(1, 2, 3, 4, 5, 6), nrow = 3, ncol = 2),
+                          init_probs = c(1, 0, 0))),
+                 matrix(c(626/9, 1652/9, 1652/9, 4442/9), nrow = 2))
+
+    expect_equal(var(MDPH(subint_mat = matrix(c(0.4, 0.24, 0.12,
+                                                0,   0.4,  0.2,
+                                                0,   0,    0.5),
+                                              ncol = 3,
+                                              byrow = TRUE),
+                          reward_mat = matrix(c(1, 2, 3, 4, 5, 6), nrow = 3, ncol = 2),
+                          init_probs = c(1, 0, 0)),
+                     v = 2),
+                 4442/9)
+
+    expect_equal(var(MDPH(subint_mat = matrix(c(0.4, 0.24, 0.12,
+                                                0,   0.4,  0.2,
+                                                0,   0,    0.5),
+                                              ncol = 3,
+                                              byrow = TRUE),
+                          reward_mat = matrix(c(1, 2, 3, 4, 5, 6), nrow = 3, ncol = 2),
+                          init_probs = c(1, 0, 0)),
+                     v = c(1, 2)),
+                 1652/9)
+
+    expect_error(var(MDPH(subint_mat = matrix(c(0.4, 0.24, 0.12,
+                                                0,   0.4,  0.2,
+                                                0,   0,    0.5),
+                                              ncol = 3,
+                                              byrow = TRUE),
+                          reward_mat = matrix(c(1, 2, 3, 4, 5, 6), nrow = 3, ncol = 2),
+                          init_probs = c(1, 0, 0)),
+                     v = c(1, 2, 1)),
+                 'right indices')
+
+
   })
+
+
+
+
+
+
+
+
+
